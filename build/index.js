0b401a752777553033b9ce45e7e4c9d1bdacf9f9
Johnny Qu <johnnyqu4@gmail.com>
2020-08-08 15:41:29 -0400

    graphql setup

--- build/index.js
+++ build/index.js
@@ -1,9 +1,23 @@
-const express = require("express");
-const app = express();
-const port = 420;
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-app.listen(port, () => {
-    console.log("server started on port " + port);
-});
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
